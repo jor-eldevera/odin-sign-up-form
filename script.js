091bc1a7198629b8d79da86e3959a40e8db2dc4e
jor-eldevera <jorell.devera@gmail.com>
2022-08-17 18:27:25 -0700

    Change password borders to gray when inputs are both empty

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var password = document.getElementById('password');
 var confirm_password = document.getElementById('confirm_password');  
 
 var check = function() {
-    console.log(password);
     if (password.value === confirm_password.value) {
         password.style.border = "1px solid green";
         confirm_password.style.border = "1px solid green";
     } else {
         password.style.border = "1px solid red";
         confirm_password.style.border = "1px solid red";
+    }
+
+    if (password.value === "" && confirm_password.value === "") {
+        password.style.border = "1px solid #E5E7EB";
+        confirm_password.style.border = "1px solid #E5E7EB";
     }
 }
